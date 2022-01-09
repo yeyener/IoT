95132a08b0e3df1d8cc2c4bb85eba9bcf8b69835
Yunus Emre Yener <y.yener@papara.com>
2022-01-09 15:10:17 +0300

    send messages and update twin properties from device

--- IoT.AgentDevice/Program.cs
+++ IoT.AgentDevice/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using IoT.AgentDevice;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Newtonsoft.Json;
+using System.Text;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello from agent!");
+
+var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
+
+string DeviceConnectionString = config.GetSection("devices").GetSection("device-01").GetSection("connectionString").Value;
+
+var device = DeviceClient.CreateFromConnectionString(DeviceConnectionString);
+
+await device.OpenAsync();
+
+Console.WriteLine("Device is connected");
+
+
+var twinProperties = new TwinCollection();
+
+// do not user space period $ unciode. use only letters and -
+twinProperties["conntecitonType"] = "wi-fi";
+twinProperties["conntecitonStrength"] = "weak";
+
+await device.UpdateReportedPropertiesAsync(twinProperties);
+
+
+//var telemetry = new Telemetry()
+//{
+//    Message = "Complex obj ",
+//    StatusCode = 1
+//};
+
+//var telemertyJson = JsonConvert.SerializeObject(telemetry);
+
+//var telemertyBytes = Encoding.ASCII.GetBytes(telemertyJson);
+
+//var message = new Message(telemertyBytes);
+
+//await device.SendEventAsync(message);
+
+//Console.WriteLine("Message sent from device");
---
+++ IoT.AgentDevice/Telemetry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT.AgentDevice
+{
+    public class Telemetry
+    {
+        public string Message { get; set; }
+
+        public int StatusCode { get; set; }
+    }
+}
